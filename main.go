9cc36dfa062fdd33cc77af00790547e5fc558053
Yehuda Ringler <ybringler@gmail.com>
2019-06-25 19:29:45 -0400

    remove user agent

--- main.go
+++ main.go
@@ -1,20 +1,19 @@
 package main
 
 import (
-	"github.com/gocolly/colly"
 	"fmt"
+
+	"github.com/gocolly/colly"
 )
 
 func main() {
-	c := colly.NewCollector(
-		colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"),
-	)
+	c := colly.NewCollector()
 
 	c.OnError(func(_ *colly.Response, err error) {
 		fmt.Println("Something went wrong:", err)
 	})
 
-	c.OnHTML("body.home #main-menu-fst > li > a ", func (e *colly.HTMLElement)  {
+	c.OnHTML("body.home #main-menu-fst > li > a ", func(e *colly.HTMLElement) {
 		fmt.Println(e.Attr("href"))
 		fmt.Println(e.Text)
 	})
