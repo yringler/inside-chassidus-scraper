0504abd6c69e608f5c3e92b64b60e349c494b337
Yehuda Ringler <yehuda.ringler@chabad.org>
2019-06-26 11:38:09 -0400

    First shot at traverses entire site, gets all sections !!!

--- insidescraper.go
+++ insidescraper.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"errors"
+	"fmt"
+
+	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly"
 )
 
 // InsideScraper scrapes insidechassidus for lesson structure.
 type InsideScraper struct {
 	activeSection *SiteSection
-	site []SiteSection
+	site          []SiteSection
+	collector     *colly.Collector
 }
 
 // Site returns the data which represents the site/lesson structure.
@@ -31,57 +35,77 @@ func (scraper *InsideScraper) Scrape() (err error) {
 		}
 	}()
 
-	c := colly.NewCollector(
+	scraper.collector = colly.NewCollector(
 		colly.UserAgent("inside-scraper"),
+		colly.AllowedDomains("http://insidechassidus.org"),
 	)
 
-	c.OnError(func(_ *colly.Response, err error) {
-		panic("Scrape error: " + err.Error())
+	scraper.collector.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Scrape error: " + err.Error())
+		fmt.Println("Url: ", r.Request.URL.RawPath)
 	})
 
-	c.OnHTML("body.home #main-menu-fst > li > a ", func(e *colly.HTMLElement) {
+	scraper.collector.OnHTML("body.home #main-menu-fst > li > a ", func(e *colly.HTMLElement) {
 		scraper.site = append(scraper.site, SiteSection{
 			Title:    e.Text,
 			Sections: make([]SiteSection, 0, 20),
 		})
 
 		scraper.activeSection = &scraper.site[len(scraper.site)-1]
-		err := c.Visit(e.Attr("href"))
+		err := scraper.collector.Visit(e.Attr("href"))
 
 		if err != nil {
-			panic("Vist main section error: " + err.Error())
+			panic("Visit main section error: " + err.Error())
 		}
 	})
 
-	// Process sub sections
-	c.OnHTML("tbody tr td:nth-child(2)", func(e *colly.HTMLElement) {
-		parentOfCurrentSection := scraper.activeSection
+	scraper.collector.OnHTML("tbody tr", func(e *colly.HTMLElement) {
+		domParent := e.DOM
 
-		domParent := e.DOM.Closest("tr")
-		domName := domParent.Find("td:first-child a")
-		domDescription := domParent.Find("td:nth-child(2)")
+		firstColumn := domParent.Find("td:nth-child(1)")
+		secondColumn := domParent.Find("td:nth-child(2)")
+		thirdColumn := domParent.Find("td:nth-child(3)")
 
-		parentOfCurrentSection.Sections = append(parentOfCurrentSection.Sections, SiteSection{
-			Title:       domName.Text(),
-			Description: domDescription.Text(),
-			Sections:    make([]SiteSection, 0, 20),
-			Lessons:     make([]Lesson, 0, 20),
-		})
-
-		scraper.activeSection = &parentOfCurrentSection.Sections[len(parentOfCurrentSection.Sections)-1]
-		sectionURL, exists := domName.Attr("href")
-
-		if !exists {
-			parentHTML, _ := domParent.Html()
-			childHTML, _ := domName.Html()
-			panic("Error! no href!\nParent: " + parentHTML + "\nchild:\n" + childHTML)
+		// If this row has only 2 columns and it doesn't describe a class
+		// than it must be a section.
+		if thirdColumn.Length() == 0 && secondColumn.Find("[mp3],a[href$=\".pdf\"]").Length() == 0 {
+			scraper.loadSection(firstColumn, secondColumn)
 		}
-
-		c.Visit(sectionURL)
-		scraper.activeSection = parentOfCurrentSection
 	})
 
-	c.Visit("http://insidechassidus.org/")
+	scraper.collector.Visit("http://insidechassidus.org/")
 
 	return err
+}
+
+func (scraper *InsideScraper) loadSection(firstColumn, domDescription *goquery.Selection) {
+	// This method creates a new section, and changes the active section to it.
+	// After it is finished, it restores the active session to the parent.
+	parentOfNewSection := scraper.activeSection
+
+	// The name of the section. A link.
+	domName := firstColumn.Find("a")
+
+	sectionURL, exists := domName.Attr("href")
+	if !exists {
+		parentHTML, _ := firstColumn.Closest("tr").Html()
+		childHTML, _ := firstColumn.Html()
+		panic("Error! no href!\nParent: " + parentHTML + "\nchild:\n" + childHTML)
+	}
+
+	parentOfNewSection.Sections = append(parentOfNewSection.Sections, SiteSection{
+		Title:       domName.Text(),
+		Description: domDescription.Text(),
+		Sections:    make([]SiteSection, 0, 20),
+		Lessons:     make([]Lesson, 0, 20),
+	})
+
+	newSection := &parentOfNewSection.Sections[len(parentOfNewSection.Sections)-1]
+
+	scraper.activeSection = newSection
+
+	scraper.collector.Visit(sectionURL)
+
+	// Restore active section.
+	scraper.activeSection = parentOfNewSection
 }
